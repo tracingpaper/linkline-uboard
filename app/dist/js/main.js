0913bd4790d402789bbbec4765ee7eddd0f88678
arjunatlast <arjunatlast@gmail.com>
2019-01-18 14:27:56 +0530

    Gallery Load more added

--- app/dist/js/main.js
+++ app/dist/js/main.js
@@ -1,3 +1,25 @@
+(function ($){
+
+    var MAX_ITEMS = 10;
+    const MORE_ITEMS = 5;
+
+    const $items = $('.gallery-item')
+
+    $items.addClass("hidden");
+
+    $items.slice(0, MAX_ITEMS).removeClass("hidden");
+
+    $('#gallery-load-more').click(function (e) {
+        $items.slice(MAX_ITEMS, MAX_ITEMS += MORE_ITEMS).removeClass("hidden");
+        //reinit aos
+        AOS.init();
+        //
+        $(this).trigger('blur');
+
+        if(MAX_ITEMS >= $items.length) $(this).remove();
+    });
+
+})(jQuery);
 const bootstrap = function ($) {
 
     //WOW js
